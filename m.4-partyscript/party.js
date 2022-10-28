23bec6de2b0b287c04b7562a1289147f291e8291
tasneemalshaikh9 <114865275+tasneemalshaikh9@users.noreply.github.com>
2022-10-28 20:01:33 +0200

    Add files via upload

---
+++ m.4-partyscript/party.js
@@ -0,0 +1,19 @@
+// Security Gaurd, allows people to enter to a party, only people between 18 and 35 years old can enter the party
+// Loop through number of people.
+do {
+    // ask each user about his/her age.
+    let age = prompt("What is your age?");
+    // if younger than 18, print: you are too young
+    if (age<18) {
+        greeting = "you are too young";
+      } 
+    // if older than 35, print: you are too old
+    else if (age>35) {
+        greeting = "you are too old";
+      }
+    // if between 18 and 35, ask the user about his/her name and welcome the user to the party.
+   else {
+        greeting = "welcome to the party";
+      }
+} while (confirm("is there anyone else?"));
+console.log(1);
